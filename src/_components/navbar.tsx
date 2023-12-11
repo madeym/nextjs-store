1bdfc3ae999439a7fd45eed5924fd243e660d791
Made <yoga.mahendra@atomic.id>
2023-12-11 15:54:05 +0700

    update slogan

--- src/_components/navbar.tsx
+++ src/_components/navbar.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { useEffect } from "react";
 
 export default function Navbar() {
@@ -33,24 +32,31 @@ export default function Navbar() {
     }
 
     return (
-        <nav className="flex py-4 px-[150px] items-center justify-between text-[24px] text-gray-600">
+        <nav className="flex py-4 px-4 lg:px-20 items-center justify-between text-[18px] lg:text-[24px] text-gray-600">
             <div className="left-nav flex gap-4 items-center">
                 <h2 className="text-[30px] font-semibold">Store</h2>
+                <div className="flex items-center relative w-100">
+                    <input type="text" placeholder="Search ..." className="w-full text-[12px] md:text-[18px] lg:text-[24px] border-2 rounded-full shadow-[0_3px_10px_rgb(0,0,0,0.2)] p-2 px-4" />
+                    <i className="bi bi-search absolute end-4 text-gray-400 text-[12px] md:text-[18px] lg:text-[24px] cursor-pointer hover:scale-105"></i>
+                </div>
             </div>
-            <div className="flex gap-[50px] items-center">
-                <span onClick={() => ScrollToSection("NewReleaseSection")} className="text-[30px] font-semibold cursor-pointer">New Release</span>
-                <span onClick={() => ScrollToSection("ProductSection")} className="text-[30px] font-semibold cursor-pointer">Product</span>
+            <div className="flex gap-4 lg:gap-[50px] items-center">
+                <span onClick={() => ScrollToSection("NewReleaseSection")} className="hidden md:block font-semibold cursor-pointer">New Release</span>
+                <span onClick={() => ScrollToSection("ProductSection")} className="hidden md:block font-semibold cursor-pointer">Product</span>
                 <div className="flex gap-2">
                     <button onClick={() => ChangeThemeMode("Dark")} type="button" className="hs-dark-mode-active:hidden hs-dark-mode group flex items-center hover:text-blue-600 font-medium  dark:hover:text-gray-500" data-hs-theme-click-value="dark">
-                        <svg className="flex-shrink-0 w-8 h-8" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" /></svg>
+                        <svg className="flex-shrink-0 w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" /></svg>
                     </button>
-                    <button onClick={() => ChangeThemeMode("Light")} type="button" className="hs-dark-mode-active:block hs-dark-mode group flex items-center hover:text-blue-600 font-medium  dark:hover:text-gray-500" data-hs-theme-click-value="light">
-                        <svg className="flex-shrink-0 w-8 h-8" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="4" /><path d="M12 8a2 2 0 1 0 4 4" /><path d="M12 2v2" /><path d="M12 20v2" /><path d="m4.93 4.93 1.41 1.41" /><path d="m17.66 17.66 1.41 1.41" /><path d="M2 12h2" /><path d="M20 12h2" /><path d="m6.34 17.66-1.41 1.41" /><path d="m19.07 4.93-1.41 1.41" /></svg>
-                    </button>
+                    {/* <button onClick={() => ChangeThemeMode("Light")} type="button" className="hs-dark-mode-active:block hs-dark-mode group flex items-center hover:text-blue-600 font-medium  dark:hover:text-gray-500" data-hs-theme-click-value="light">
+                        <svg className="flex-shrink-0 w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="4" /><path d="M12 8a2 2 0 1 0 4 4" /><path d="M12 2v2" /><path d="M12 20v2" /><path d="m4.93 4.93 1.41 1.41" /><path d="m17.66 17.66 1.41 1.41" /><path d="M2 12h2" /><path d="M20 12h2" /><path d="m6.34 17.66-1.41 1.41" /><path d="m19.07 4.93-1.41 1.41" /></svg>
+                    </button> */}
                 </div>
                 <div className="flex items-center gap-4">
-                    <span className="text-[30px] font-semibold">EN</span>
-                    <i className="!text-[30px] bi bi-globe-americas"></i>
+                    <span className=" font-semibold">EN</span>
+                    <i className="bi bi-globe-americas"></i>
+                </div>
+                <div className="md:hidden">
+                    <i className="bi bi-list"></i>
                 </div>
             </div>
         </nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,69 +3,80 @@ import Navbar from '@/_components/navbar'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css/bundle'
 import { Autoplay, EffectFade } from 'swiper/modules'
+import Typed from 'typed.js'
+import { useEffect } from 'react'
 
 export default function Home() {
+  useEffect(() => {
+    const slogan = new Typed('#sloganText', {
+      strings: ['FIND YOUR <i>Character</i>', 'FIND YOUR <i>Style</i>', 'YOUR <br> EVERYDAY <br> FASHION.',],
+      typeSpeed: 50,
+      backSpeed: 50,
+      showCursor: false
+    });
+
+    return () => {
+      slogan.destroy();
+    }
+  }, [])
+
   return (
     <main>
       <Navbar />
       <div className='w-100 relative'>
-        <div className='absolute flex flex-col top-[300px] left-[100px]'>
-          <span className='text-[80px] font-semibold'>YOUR</span>
-          <span className='text-[80px] font-semibold'>EVERYDAY</span>
-          <span className='text-[80px] font-semibold'>FASHION.</span>
-          <div className="flex items-center relative w-100">
-            <input type="text" placeholder="Search ..." className="w-full text-[24px] border-2 rounded-full shadow-[0_3px_10px_rgb(0,0,0,0.2)] p-2 px-4" />
-            <i className="bi bi-search absolute end-4 text-gray-400 text-[24px] cursor-pointer hover:scale-105"></i>
-          </div>
+        <div className='absolute h-[100%] flex left-10 items-center lg:left-[100px]'>
+          <span id='sloganText' className='text-[24px] md:text-[30px] lg:text-[50px] font-semibold'></span>
         </div>
 
         <img src="/assets/homepage-top.jpg" alt="" />
       </div>
 
-      <h2 id='ProductSection' className='text-[40px] font-semibold mt-[40px] text-center'>Our Products</h2>
+      {/* Our Product */}
+      <h2 id='ProductSection' className='text-[24px] lg:text-[30px] font-semibold mt-8 text-center'>Our Products</h2>
 
-      <div className='w-100 flex justify-center gap-10 p-4'>
-        <div className='flex flex-col gap-2 mt-[30px] w-fit cursor-pointer transition-all hover:scale-110'>
+      <div className='w-100 flex flex-col sm:flex-row items-center justify-center sm:gap-4 lg:gap-10 p-4'>
+        <div className='flex flex-col gap-2 mt-4 w-fit cursor-pointer transition-all hover:scale-110'>
           <div>
-            <img src="/assets/hoempage-tshirt.jpg" className='h-[500px] rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]' alt="" />
+            <img src="/assets/hoempage-tshirt.jpg" className='h-[500px] sm:h-[300px] lg:h-[500px] rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]' alt="" />
           </div>
           <div className='px-2 flex flex-col items-center'>
-            <span className='font-semibold text-[30px]'>T-shirt</span>
-            <span className='font-semibold text-[20px] text-gray-400'>15 collection</span>
+            <span className='font-semibold text-[20px]'>T-shirt</span>
+            <span className='font-semibold text-[18px] text-gray-300'>15 collection</span>
           </div>
         </div>
-        <div className='flex flex-col gap-2 mt-[30px] w-fit cursor-pointer transition-all hover:scale-110'>
+        <div className='flex flex-col gap-2 mt-4 w-fit cursor-pointer transition-all hover:scale-110'>
           <div>
-            <img src="/assets/homepage-hoodie.jpg" className='h-[500px] rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]' alt="" />
+            <img src="/assets/homepage-hoodie.jpg" className='h-[500px] sm:h-[300px] lg:h-[500px] rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]' alt="" />
           </div>
           <div className='px-2 flex flex-col items-center'>
-            <span className='font-semibold text-[30px]'>Hoodie</span>
-            <span className='font-semibold text-[20px] text-gray-400'>coming soon</span>
+            <span className='font-semibold text-[20px]'>Hoodie</span>
+            <span className='font-semibold text-[18px] text-gray-300'>coming soon</span>
           </div>
         </div>
-        <div className='flex flex-col gap-2 mt-[30px] w-fit cursor-pointer transition-all hover:scale-110'>
+        <div className='flex flex-col gap-2 mt-4 w-fit cursor-pointer transition-all hover:scale-110'>
           <div>
-            <img src="/assets/homepage-totebag.jpg" className='h-[500px] rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]' alt="" />
+            <img src="/assets/homepage-totebag.jpg" className='h-[500px] sm:h-[300px] lg:h-[500px] rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]' alt="" />
           </div>
           <div className='px-2 flex flex-col items-center'>
-            <span className='font-semibold text-[30px]'>Tote Bag</span>
-            <span className='font-semibold text-[20px] text-gray-400'>coming soon</span>
+            <span className='font-semibold text-[20px]'>Tote Bag</span>
+            <span className='font-semibold text-[18px] text-gray-300'>coming soon</span>
           </div>
         </div>
       </div>
 
-      <h2 id='NewReleaseSection' className='text-[40px] font-semibold mt-[40px] text-center'>New Release</h2>
+      <h2 id='NewReleaseSection' className='text-[24px] lg:text-[30px] font-semibold mt-8 text-center'>New Release</h2>
 
-      <div className='w-[70%] mx-auto rounded-2xl flex justify-center gap-2 mt-[50px] backdrop-blur-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] mb-[100px]'>
-        <div className='w-[70%] p-4 flex flex-col'>
-          <span className='text-[40px] font-semibold'>Heartbreak Collection Vol.1</span>
-          <span className='text-justify text-[20px] mt-[20px]'>us tempus. Morbi vehicula condimentum nisl at lacinia. Sed at metus in tellus rhoncus aliquam. Sed egestas, sapien consequat faucibus efficitur, metus erat ultrices sapien, vel ornare nibh enim non mauris. Pellentesque at semper sapien. Quisque ultricies porttitor tincidunt. Donec at dolor ipsum. Aenean sodales scelerisque ipsum, vitae egestas augue. Sed condimentum turpis ut pharetra ornare. Aliquam ultrices, massa eget semper ornare, velit sapien tempus tellus, vel consectetur tellus lectus sit amet urna. Aliquam sed eros euismod, tempor mauris nec, aliquam neque. Praesent dui velit, porttitor cursus tristique eget. us tempus. Morbi vehicula condimentum nisl at lacinia. Sed at metus in tellus rhoncus aliquam. Sed egestas, sapien consequat faucibus efficitur, metus erat ultrices sapien, vel ornare nibh enim non mauris. Pellentesque at semper saeros euismod, tempor mauris nec, aliquam neque. Praesent dui velit, porttitor cursus tristique eget.</span>
-          <button className="] bg-gray-400 text-white font-bold py-2 px-4 rounded-lg w-[200px] flex gap-3 justify-center items-center mt-[20px]">
-            <i className="bi bi-arrow-right text-bold text-[24px] mt-1"></i>
-            <span className='text-[20px]'>See More</span>
+      <div className='w-[90%] lg:w-[70%] mx-auto rounded-2xl flex justify-between gap-2 mt-10 backdrop-blur-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] mb-[100px]'>
+        <div className='w-[60%] lg:w-[70%] p-4 flex flex-col'>
+          <span className='text-[20px] font-semibold'>Heartbreak Collection Vol.1</span>
+          <span className='hidden lg:block text-justify text-[18px] mt-[20px]'>us tempus. Morbi vehicula condimentum nisl at lacinia. Sed at metus in tellus rhoncus aliquam. Sed egestas, sapien consequat faucibus efficitur, metus erat ultrices sapien, vel ornare nibh enim non mauris. Pellentesque at semper sapien. Quisque ultricies porttitor tincidunt. Donec at dolor ipsum. Aenean sodales scelerisque ipsum, vitae egestas augue. Sed condimentum turpis ut pharetra ornare. Aliquam ultrices, massa eget semper ornare, velit sapien tempus tellus, vel consectetur tellus lectus sit amet urna. Aliquam sed eros euismod, tempor mauris nec, aliquam neque. Praesent dui velit, porttitor cursus tristique eget. us tempus. Morbi vehicula condimentum nisl at lacinia. Sed at metus in tellus rhoncus aliquam. Sed egestas, sapien consequat faucibus efficitur, metus erat ultrices sapien, vel ornare nibh enim non mauris. Pellentesque at semper saeros euismod, tempor mauris nec, aliquam neque. Praesent dui velit, porttitor cursus tristique eget.</span>
+          <span className='block lg:hidden text-justify text-[18px] mt-[20px]'>us tempus. Morbi vehicula condimentum nisl at lacinia. Sed at metus in tellus rhoncus aliquam. Sed egestas, sapien consequat faucibus efficitur, metus erat ultrices sapien, vel ornare nibh enim non mauris. Pellentesque at semper sapien. Quisque ultricies porttitor tincidunt. Donec at dolor ipsum. Aenean sodales scelerisque ipsum, vitae egestas augue. Sed condimentum turpis ut pharetra ornare. Aliquam ultrices, massa eget semper ornare, velit sapien tempus tellus, vel consectetur..</span>
+          <button className="] bg-gray-400 text-white font-bold py-2 px-4 rounded-lg w-[150px] flex gap-3 justify-center items-center mt-[20px]">
+            <i className="bi bi-arrow-right text-bold text-[18px] mt-1"></i>
+            <span className='text-[18px]'>See More</span>
           </button>
         </div>
-        <div className='w-[30%]'>
+        <div className='w-[40%] lg:w-[30%]'>
           <Swiper
             modules={[Autoplay, EffectFade]}
             centeredSlides={true}
